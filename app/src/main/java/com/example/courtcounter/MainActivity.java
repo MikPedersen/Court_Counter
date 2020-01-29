01c76fb94e50c041badb1efc76fe9d0812f2fe08
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2020-01-29 15:04:28 +0100

    Change alot of code

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -10,53 +10,90 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     int score_a= 0;
     int score_b= 0;
+    // Tracks the score for Team A
+    int scoreTeamA = 0;
+
+    // Tracks the score for Team B
+    int scoreTeamB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-    public void point_3_a(View view){
-        score_a= score_a + 3;
-        display_a(score_a);
     }
 
-    public void point_2_a(View view){
-        score_a= score_a + 2;
-        display_a(score_a);
-    }
-    public void point_1_a(View view){
-        score_a= score_a + 1;
-        display_a(score_a);
-    }
-    public void point_3_b(View view){
-        score_b= score_b + 3;
-        display_b(score_b);
-    }
-    public void point_2_b(View view){
-        score_b= score_b + 2;
-        display_b(score_b);
-    }
-    public void point_1_b(View view){
-        score_b= score_b + 1;
-        display_b(score_b);
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
     }
 
-    public void reset(View view){
-        score_a= 0;
-        score_b= 0;
-        display_a(0);
-        display_b(0);
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
     }
 
-    private void display_a(int number_a){
-        TextView quantityTextView_a= findViewById(R.id.score_team_a);
-        quantityTextView_a.setText("" + number_a);
-
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
     }
-    private void display_b(int number_b){
-        TextView quantityTextView_b= findViewById(R.id.score_team_b);
-        quantityTextView_b.setText("" + number_b);
 
+    /**
+     * Increase the score for Team B by 1 point.
+     */
+    public void addOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
     }
+
+    /**
+     * Increase the score for Team B by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Resets the score for both teams back to 0.
+     */
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 }
